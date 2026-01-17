589c870bc466117fd76c4ac0d2588d41b0aad5c7
dongguacute <gudupao2022@gmail.com>
2026-01-17 11:59:37 +0800

    fix: update GitHub URL in navbar config to point to user profile

--- src/config.ts
+++ src/config.ts
@@ -46,7 +46,7 @@ export const navBarConfig: NavBarConfig = {
 		LinkPreset.About,
 		{
 			name: "GitHub",
-			url: "https://github.com/dongguacute/English-Blog", // Internal links should not include the base path, as it is automatically added
+			url: "https://github.com/dongguacute", // Internal links should not include the base path, as it is automatically added
 			external: true, // Show an external link icon and will open in a new tab
 		},
 	],
